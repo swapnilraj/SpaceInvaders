e4cdb3494576f2b8e643534ec4b2889c8b333af4
Swapnil Raj <raj.swapnil99@gmail.com>
2017-02-02 15:05:02 +0000

    Remove unused constants

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -5,8 +5,6 @@ import java.awt.Color;
 public class Constants {
   public static final Color BACKGROUND_COLOR = Color.WHITE;
   public static final String ALIEN_IMAGE = "alien%d.GIF";
-  public static final String ALIEN_IMAGE_1 = "alien0.GIF";
-  public static final String ALIEN_IMAGE_3 = "alien3.jpg";
   public static final int ALIEN_HEIGHT = 40;
   public static final String ALIEN_EXPLODE_IMAGE = "explode.GIF";
   public static final int ALIEN_SPEED = 2;
