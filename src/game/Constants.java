17c581dcb6c20627b1b0161ac216c56f35678e6b
Swapnil Raj <raj.swapnil99@gmail.com>
2017-02-02 14:44:10 +0000

    Change frame count for explode

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
   public static final int ALIEN_HEIGHT = 40;
   public static final String ALIEN_EXPLODE_IMAGE = "explode.GIF";
   public static final int ALIEN_SPEED = 2;
-  public static final int FRAME_COUNT_EXPLODE = 200;
+  public static final int FRAME_COUNT_EXPLODE = 300;
   public static final int MARGIN = 20;
   public static final int DEFAULT_GROUP_SIZE = 10;
 }
