2410df7474403ad719e706c2acf2ac8ef3d4ead7
Swapnil Raj <raj.swapnil99@gmail.com>
2017-02-08 14:42:12 +0000

    Added observable pattern for mousePress
    Create new bullet objects on mousePress

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.util.ArrayList;
 import processing.core.PImage;
 import processing.core.PVector;
 
@@ -9,19 +10,31 @@ import static game.Constants.PLAYER_IMAGE;
 /**
  * Created by thawne on 07/02/17.
  */
-public class Player {
+public class Player implements game.Observer {
 
-  Space parent;
-  PImage playerImg;
-  PVector position;
+  private Space parent;
+  private PImage playerImg;
+  private PVector position;
+  private ArrayList<Bullet> bullets;
+
+  @Override
+  public void update(int index) {
+    PVector bulletPosition = new PVector(parent.mouseX, position.y);
+    bullets.add(new Bullet(parent, bulletPosition));
+  }
 
   Player(Space parent) {
     this.parent = parent;
     this.playerImg = parent.loadImage(PLAYER_IMAGE);
     this.position = new PVector(parent.mouseX, parent.height - MARGIN);
+    this.bullets = new ArrayList<>();
   }
 
   public void draw() {
     parent.image(playerImg, parent.mouseX, position.y);
+    for (Bullet bullet : bullets) {
+      bullet.draw();
+      bullet.move();
+    }
   }
 }
--- src/game/Space.java
+++ src/game/Space.java
@@ -1,14 +1,17 @@
 package game;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
 
 import static game.Constants.*;
 
-public class Space extends PApplet {
+public class Space extends PApplet implements game.Observable {
 
   private AlienGroup group1;
   private AlienGroup group2;
   private Player player;
+  private int bulletCount;
+  private ArrayList<Observer> observers;
 
   public void settings() {
     fullScreen();
@@ -18,9 +21,11 @@ public class Space extends PApplet {
     noStroke();
     noCursor();
     imageMode(CENTER);
-    group1 = new AlienGroup(this, DEFAULT_GROUP_SIZE, MARGIN);
-    group2 = new AlienGroup(this, DEFAULT_GROUP_SIZE, MARGIN * 3);
-    player = new Player(this);
+    this.observers = new ArrayList<>();
+    this.bulletCount = 0;
+    this.group1 = new AlienGroup(this, DEFAULT_GROUP_SIZE, MARGIN);
+    this.group2 = new AlienGroup(this, DEFAULT_GROUP_SIZE, MARGIN * 3);
+    this.player = new Player(this);
   }
 
   public void draw() {
@@ -32,9 +37,27 @@ public class Space extends PApplet {
 
   public void mousePressed() {
     System.out.println("TEST");
+    notifyObserver();
   }
 
   public static void main(String[] args) {
     PApplet.main(Space.class.getName());
+  }
+
+  @Override
+  public void addObserver(Observer o) {
+    observers.add(o);
+  }
+
+  @Override
+  public void removeObserver(Observer o) {
+    observers.remove(o);
+  }
+
+  @Override
+  public void notifyObserver() {
+    for (Observer observer : observers) {
+      observer.update(bulletCount++);
+    }
   }
 }
