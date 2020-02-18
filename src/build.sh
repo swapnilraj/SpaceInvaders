a0c400c2ed6142e2cd76bf875d48e136236e44f8
Swapnil Raj <raj.swapnil99@gmail.com>
2020-02-18 17:55:44 +0000

    Add "build" scrpit

---
+++ src/build.sh
@@ -0,0 +1,3 @@
+javac game/*.java game/util/*.java  -cp .:./lib/minim.jar:./lib/core.jar:./lib/jsminim.jar
+
+java  -cp .:./lib/minim.jar:./lib/core.jar:./lib/jsminim.jar:./lib/jl1.0.1.jar:./lib/mp3spi1.9.5.jar:./lib/tritonus_aos.jar:./lib/tritonus_share.jar game/Space
